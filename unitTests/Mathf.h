6351a6047887807704c0bb08977bfa1169e3003c
Michal Motyčka <mychalmotycka@gmail.com>
2016-04-25 23:19:17 +0200

    testy uprava

--- unitTests/Mathf.h
+++ unitTests/Mathf.h
@@ -1,6 +1,18 @@
+/*******************************************************************
+ * Název projektu: Kalkulačka
+ * Soubor: Mathf.h
+ * Datum: 24.4.2016
+ * Autor: skupina 3++
+ * Popis: Matematická knihovna
+ *******************************************************************/
+/**
+* @file Mathf.h
+* @author skupina 3++
+* @brief  Knihovna pro matematické operace.
+*/
 #ifndef MATHF_H
 #define MATHF_H
-
+#include <math.h>
 #include<string>
 	class Mathf
 	{
@@ -14,3 +26,4 @@
 		static double sqrt(double);
 	};
 #endif
+/*** Konec souboru Mathf.h ***/
--- unitTests/Qlabel.cpp
+++
@@ -1,8 +0,0 @@
-#include "Qlabel.h"
-using namespace std;
-string Qlabel::text() {
-	return value;
-}
-void Qlabel::text(const string& text) {
-	value = text;
-}
