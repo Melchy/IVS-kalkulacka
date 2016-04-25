7d1ef3a8b3383edf897cf39332752e21eeea48e2
NelaMm <macurova.nela@seznam.cz>
2016-04-25 23:52:23 +0200

    Update Qlabel.cpp

--- kalkul/Qlabel.cpp
+++ kalkul/Qlabel.cpp
@@ -1,3 +1,15 @@
+/*******************************************************************
+ * Název projektu: Kalkulačka
+ * Soubor: Qlabel.cpp
+ * Datum: 24.4.2016
+ * Autor: skupina 3++
+ * Popis: Třída Qlabel
+ *******************************************************************/
+/**
+* @file Qlabel.cpp
+* @author skupina 3++
+* @brief Třída Qlabel
+*/
 #include "Qlabel.h"
 using namespace std;
 string Qlabel::text() {
@@ -6,3 +18,4 @@ string Qlabel::text() {
 void Qlabel::text(const string& text) {
 	value = text;
 }
+/*** Konec souboru Qlabel.cpp ***/
