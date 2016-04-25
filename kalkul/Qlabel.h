6a01a32f2d5f384c85247d148f6d92a4e847eb89
NelaMm <macurova.nela@seznam.cz>
2016-04-25 23:53:41 +0200

    Update Qlabel.h

--- kalkul/Qlabel.h
+++ kalkul/Qlabel.h
@@ -1,3 +1,15 @@
+/*******************************************************************
+ * Název projektu: Kalkulačka
+ * Soubor: Qlabel.h
+ * Datum: 24.4.2016
+ * Autor: skupina 3++
+ * Popis: Třída Qlabel
+ *******************************************************************/
+/**
+* @file Qlabel.h
+* @author skupina 3++
+* @brief Třída Qlabel
+*/
 #ifndef QLABEL_H
 #define QLABEL_H
 	#include <string>
@@ -10,3 +22,4 @@
 		std::string value;
 	};
 #endif
+/*** Konec souboru Qlabel.h ***/
