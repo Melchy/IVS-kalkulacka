06b5b8fe3cb8470b1f726cbaef6c3266d6290ca0
NelaMm <macurova.nela@seznam.cz>
2016-04-25 17:33:27 +0200

    Update main.cpp

--- kalkul/main.cpp
+++ kalkul/main.cpp
@@ -1,7 +1,14 @@
+/*******************************************************************
+ * Název projektu: Kalkulačka
+ * Soubor: main.cpp
+ * Datum: 24.4.2016
+ * Autor: skupina 3++
+ * Popis: Třída která vytvoří aplikaci
+ *******************************************************************/
 /**
 * @file main.cpp
 * @author skupina 3++
-* @brief  Hlavny soubor.
+* @brief  Hlavní soubor vygenerován Qt Creator
 */
 #include "mainwindow.h"
 #include "Operations.h"
@@ -9,13 +16,19 @@
 #include <QApplication>
 #include <QLabel>
 
+/**
+ * Vytvoření aplikace, hlavního okna a zobrazení
+ * @param argc počet argumentů
+ * @param argv argumenty(nazev programu)
+ * @return výstup aplikace
+ */
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication a(argc, argv); //vytvoření aplikace
+    MainWindow w; //vytvoření hlavního okna
+    w.show(); //zobrazení hlavního okna na obrazovku
 
-    return a.exec();
+    return a.exec();//spuštění aplikace
 }
 /*** Konec souboru main.cpp ***/
